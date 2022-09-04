8d449120bb8fba5c0a9c7d09d5d1350141c788cd
yordan-marinov <jordanmarinov8@gmail.com>
2022-09-04 16:29:48 +0300

    Created LibraryEventProducer

---
+++ event_producer/src/main/java/com/yordanm/event_producer/producer/LibraryEventProducer.java
@@ -0,0 +1,53 @@
+package com.yordanm.event_producer.producer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yordanm.event_producer.domains.LibraryEvent;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+@Component
+@Slf4j
+@AllArgsConstructor
+public class LibraryEventProducer {
+
+    private KafkaTemplate<Integer, String> kafkaTemplate;
+    private ObjectMapper objectMapper;
+
+    public void sendLibraryEvent(LibraryEvent libraryEvent) throws JsonProcessingException {
+        int key = libraryEvent.getEventId();
+        String value = objectMapper.writeValueAsString(libraryEvent);
+
+        ListenableFuture<SendResult<Integer, String>> listenableFuture = kafkaTemplate.sendDefault(key, value);
+        listenableFuture.addCallback(new ListenableFutureCallback<>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                handleFailure(ex);
+            }
+
+            @Override
+            public void onSuccess(SendResult<Integer, String> result) {
+                handleSuccess(key, value, result);
+            }
+        });
+    }
+
+    private void handleSuccess(int key, String value, SendResult<Integer, String> result) {
+        log.info("Message send successfully for the key: {} with the value: {}, partion is: {}",
+                key, value, result.getRecordMetadata().partition());
+    }
+
+    private void handleFailure(Throwable ex) {
+        log.error("Error Sending Message with exception: {}", ex.getMessage());
+        try {
+            throw ex;
+        } catch (Throwable throwable) {
+            log.error("Error in OnFailure: {}", throwable.getMessage());
+        }
+    }
+}
