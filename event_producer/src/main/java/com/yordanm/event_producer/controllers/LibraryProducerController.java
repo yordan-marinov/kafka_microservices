a5d3ab43cb479131a23564b8e45406a445322d99
yordan-marinov <jordanmarinov8@gmail.com>
2022-09-04 15:55:50 +0300

    Added Service package

--- event_producer/src/main/java/com/yordanm/event_producer/controllers/LibraryProducerController.java
+++ event_producer/src/main/java/com/yordanm/event_producer/controllers/LibraryProducerController.java
@@ -1,6 +1,9 @@
 package com.yordanm.event_producer.controllers;
 
 import com.yordanm.event_producer.domain.LibraryEvent;
+import com.yordanm.event_producer.service.LibraryEventProducerService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,11 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
+@NoArgsConstructor
 @RequestMapping("/api/v1/libraryEvent")
 public class LibraryProducerController {
 
+    private LibraryEventProducerService producerService;
+
     @PostMapping
     public ResponseEntity<LibraryEvent> postLibraryEvents(@RequestBody LibraryEvent libraryEvent) {
+        producerService.produce(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
---
+++ event_producer/src/main/java/com/yordanm/event_producer/service/LibraryEventProducerService.java
@@ -0,0 +1,12 @@
+package com.yordanm.event_producer.service;
+
+import com.yordanm.event_producer.domain.LibraryEvent;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LibraryEventProducerService {
+
+
+    public void produce(LibraryEvent libraryEvent) {
+    }
+}
